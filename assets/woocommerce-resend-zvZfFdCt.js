53e17f0f373404a3b76e68a4d869f1cacc2f6232
huy.pham@silentium.io <huy.pham@silentium.io>
2024-08-03 22:36:04 +0700

    Updates

---
+++ assets/woocommerce-resend-zvZfFdCt.js
@@ -0,0 +1,132 @@
+import{u as r,j as n}from"./index-kCuBO1eh.js";const l={title:"3000 mails free per month for woocommerce",description:"woocommerce integrate with resend.com"};function s(i){const e={a:"a",div:"div",em:"em",h1:"h1",h2:"h2",h3:"h3",header:"header",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...r(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.header,{children:n.jsxs(e.h1,{id:"3000-mails-free-per-month-for-woocommerce",children:[n.jsx(e.strong,{children:"3000 mails free per month for woocommerce"}),n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#3000-mails-free-per-month-for-woocommerce",children:n.jsx(e.div,{"data-autolink-icon":!0})})]})}),`
+`,n.jsxs(e.h2,{id:"overview-the-purpose-the-problem-it-is-solving",children:[n.jsx(e.strong,{children:"Overview the purpose, the problem it is solving"}),n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview-the-purpose-the-problem-it-is-solving",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsx(e.p,{children:"For SME woocommerce, we need to send email to customer/shop admin, when woocommerce order/invoice/account are changed."}),`
+`,n.jsxs(e.h2,{id:"proposal",children:[n.jsx(e.strong,{children:"Proposal"}),n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#proposal",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"On resend.com, Register resend.com account, verify domain, create API key"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"On wordpress website, install and configure Mail SMTP Plugin, manage Woocommerce email settings"}),`
+`]}),`
+`]}),`
+`,n.jsxs(e.h2,{id:"weighting--pros-and-cons",children:[n.jsx(e.strong,{children:"Weighting / Pros and cons"}),n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#weighting--pros-and-cons",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.h3,{id:"pros",children:["Pros",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#pros",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Free 3000 mails per month"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Not spam mail"}),`
+`]}),`
+`]}),`
+`,n.jsxs(e.h3,{id:"cons",children:["Cons",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cons",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Free version of resend account allows creating only 1 domains, it should be your main domain as a developer/company"}),`
+`]}),`
+`,n.jsxs(e.h2,{id:"detail-design--implementation-steps",children:[n.jsx(e.strong,{children:"Detail design / implementation steps"}),n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#detail-design--implementation-steps",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.h3,{id:"register-a-resend-service-account",children:["Register a Resend service account",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#register-a-resend-service-account",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.p,{children:["go to: ",n.jsx(e.a,{href:"https://resend.com",children:"https://resend.com"})]}),`
+`,n.jsxs(e.h3,{id:"create-resend-api-key",children:["Create Resend API key",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#create-resend-api-key",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.p,{children:["go to: ",n.jsx(e.a,{href:"https://resend.com/api-keys",children:"https://resend.com/api-keys"})]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/resend-create-API-key.png",alt:"resend-create-API-key.png"})}),`
+`,n.jsxs(e.h3,{id:"register-domain",children:["Register domain",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#register-domain",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.p,{children:["go to: ",n.jsx(e.a,{href:"https://resend.com/domains",children:"https://resend.com/domains"})]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/resend-domain-detail.png",alt:"resend-domain-detail.png"})}),`
+`,n.jsxs(e.h3,{id:"download-wordpress-mail-smtp-plugin",children:["Download WordPress Mail SMTP Plugin",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#download-wordpress-mail-smtp-plugin",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsx(e.p,{children:n.jsx(e.a,{href:"https://vi.wordpress.org/plugins/wp-mail-smtp/",children:"https://vi.wordpress.org/plugins/wp-mail-smtp/"})}),`
+`,n.jsxs(e.h3,{id:"install-mail-smtp-plugin",children:["Install Mail SMTP Plugin",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#install-mail-smtp-plugin",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ol,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Login into your wordpress admin"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Install plugin in admin site"}),`
+`]}),`
+`]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"On the left menu: Plugins -> Add New Plugin -> Upload plugin"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Select plugin downloaded from step 1 -> Install now"}),`
+`]}),`
+`]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/wp-install-plugin.png",alt:"wp-install-plugin.png"})}),`
+`,n.jsxs(e.ol,{start:"3",children:[`
+`,n.jsx(e.li,{children:"Update Mail SMPT config"}),`
+`]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(e.p,{children:["When install plugin successfully, on the left menu will display item ",n.jsx(e.em,{children:"WP Mail SMTP"})]}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Update to these configs"}),`
+`]}),`
+`]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"License Key: Free version, empty value"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(e.p,{children:["From Email: Must have domain is registerd on resend.com/domains, exp: ",n.jsx(e.a,{href:"mailto:admin@quochuy.dev",children:"admin@quochuy.dev"})]}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"From Name: (can be your branding or your website)"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Force From Name: default false"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Return Path: default false"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Mailer: Other SMTP"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"SMTP Host: smtp.resend.com"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Encryption: TLS"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"SMTP Port: 587"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Authentication: On"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"SMTP Username: resend"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"SMTP Password: (value is from API key created on Resend)"}),`
+`]}),`
+`]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["Click ",n.jsx(e.em,{children:"Save settings"})]}),`
+`]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/wp-update-smtp-config.png",alt:"wp-update-smtp-config.png"})}),`
+`,n.jsxs(e.h3,{id:"define-email-notifications",children:["Define email notifications",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#define-email-notifications",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"On the left menu: WooCommerce -> Settings -> Emails"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Manage Email notifications"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(e.p,{children:["Click ",n.jsx(e.em,{children:"Save changes"})]}),`
+`]}),`
+`]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/woocommerce-setting-mail-1.png",alt:"woocommerce-setting-mail-1.png"})}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/woocommerce-setting-mail-2.png",alt:"woocommerce-setting-mail-2.png"})}),`
+`,n.jsxs(e.h3,{id:"testing",children:["Testing",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#testing",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"On woocommerce website, add to cart products then checkout order"}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsx(e.p,{children:"Check admin gmail"}),`
+`]}),`
+`]}),`
+`,n.jsx(e.p,{children:n.jsx(e.img,{src:"/mail-template.png",alt:"woocommerce-setting-mail-1.png"})})]})}function t(i={}){const{wrapper:e}={...r(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(s,{...i})}):s(i)}export{t as default,l as frontmatter};
