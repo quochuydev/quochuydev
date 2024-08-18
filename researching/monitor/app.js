3ab3a589e15cb80f8b83ae730fda876cc9a81cd1
quochuydev <quochuy.dev@gmail.com>
2024-08-18 09:28:12 +0700

    🚄

--- researching/monitor/app.js
+++ researching/monitor/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+
+const tracer = require("./tracing");
+
 const app = express();
 const port = 3001;
 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/rolldice", (req, res) => {
+  const span = tracer.startSpan("root_span");
+  const result = getRandomNumber(1, 6).toString();
+  span.setAttribute("result", result);
+  span.end();
+  res.send(result);
 });
 
 app.listen(port, () => {
---
+++ researching/monitor/tracing.js
@@ -0,0 +1,37 @@
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const opentelemetry = require("@opentelemetry/api");
+const SemanticResourceAttributes = require("@opentelemetry/semantic-conventions");
+const { Resource } = require("@opentelemetry/resources");
+const {
+  SEMRESATTRS_SERVICE_NAME,
+} = require("@opentelemetry/semantic-conventions");
+const {
+  diag,
+  trace,
+  context,
+  DiagConsoleLogger,
+  DiagLogLevel,
+} = require("@opentelemetry/api");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "myapp",
+  [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: "instance-id",
+  [SemanticResourceAttributes.SERVICE_VERSION]: "v1.0.0",
+});
+
+const exporter = new OTLPTraceExporter({
+  url: "http://localhost:4318/v1/traces", // OTLP/HTTP endpoint for Tempo
+});
+
+const provider = new NodeTracerProvider({ resource });
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.register();
+
+const tracer = trace.getTracer("nodejs-tracing-app");
+
+module.exports = tracer;
