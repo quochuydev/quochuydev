72a85d7520c041ce47abbd8566abe0e525cea5eb
quochuydev <quochuy.dev@gmail.com>
2024-08-18 10:55:24 +0700

    🚄

--- researching/monitor/app.js
+++ researching/monitor/app.js
@@ -16,8 +16,10 @@ app.get("/", (req, res) => {
 app.get("/rolldice", (req, res) => {
   const span = tracer.startSpan("root_span");
   const result = getRandomNumber(1, 6).toString();
+  console.log("loki:result", result);
   span.setAttribute("result", result);
   span.end();
+  res.header("x-myapp-trace-id", span.spanContext().traceId);
   res.send(result);
 });
 
--- researching/monitor/tracing.js
+++ researching/monitor/tracing.js
@@ -2,7 +2,9 @@ const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 const opentelemetry = require("@opentelemetry/api");
-const SemanticResourceAttributes = require("@opentelemetry/semantic-conventions");
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
 const { Resource } = require("@opentelemetry/resources");
 const {
   SEMRESATTRS_SERVICE_NAME,
