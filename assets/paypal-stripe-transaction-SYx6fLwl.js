b916c875c6b4910211449e0dd99ac334350ca556
quochuydev <quochuydev@users.noreply.github.com>
2025-03-09 14:30:47 +0000

    deploy: 0ab1b8f3d780dc3c9cda2f16938712cf24a08097

--- assets/paypal-stripe-transaction-SYx6fLwl.js
+++
@@ -1,5 +0,0 @@
-import{u as r,j as n}from"./index-BugPor3d.js";const c=void 0;function o(t){const e={p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.p,{children:"Loading..."}),`
-`,n.jsx(e.p,{children:"."}),`
-`,n.jsx(e.p,{children:"."}),`
-`,n.jsx(e.p,{children:"."}),`
-`,n.jsx(e.p,{children:"...Forever."})]})}function i(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{i as default,c as frontmatter};
