c607209475a0bd493529719db2d6a0c32d6c1121
quochuydev <quochuy.dev@gmail.com>
2025-07-24 13:40:30 +0700

    🌱

--- researching/ai/main.ts
+++ researching/ai/main.ts
@@ -1,13 +1,11 @@
+import { CSVLoader } from "@langchain/community/document_loaders/fs/csv";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import dotenv from "dotenv";
 import fs from "fs";
-import path from "path";
-
+import { JSONLoader } from "langchain/document_loaders/fs/json";
 import { MultiFileLoader } from "langchain/document_loaders/fs/multi_file";
-import {
-  JSONLoader,
-  JSONLinesLoader,
-} from "langchain/document_loaders/fs/json";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import path from "path";
 
 if (process.env.ENV_PATH) {
   const defaultConfig = dotenv.parse(
@@ -18,9 +16,20 @@ if (process.env.ENV_PATH) {
   });
 }
 
-const loader = new MultiFileLoader(["./example_data/movies.json"], {
-  ".json": (path) => new JSONLoader(path),
-});
+const loader = new MultiFileLoader(
+  [
+    // "./example_data/movies.json",
+    "./example_data/care-providers.csv",
+    "./example_data/security-key.pdf",
+    "./example_data/teddy.jpeg",
+  ],
+  {
+    ".json": (path) => new JSONLoader(path),
+    ".pdf": (path) => new PDFLoader(path),
+    ".txt": (path) => new TextLoader(path),
+    ".csv": (path) => new CSVLoader(path),
+  }
+);
 
 const docs = await loader.load();
-console.log({ docs: JSON.stringify(docs, null, 2) });
+console.log({ docs: JSON.stringify(docs) });
