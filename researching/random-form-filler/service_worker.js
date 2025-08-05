f0c9b92ad0e4572c191b0e9bfaf8b0f7b856dacd
quochuydev <quochuy.dev@gmail.com>
2025-08-05 16:11:46 +0700

    chore: remove extension

--- researching/random-form-filler/service_worker.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-});
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === "PING") {
-    sendResponse({ type: "PONG", sender, time: Date.now() }); // ← PONG here
-  }
-});
