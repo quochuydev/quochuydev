109b8aaf28343a959e4764fd4f890e2cd78d4a19
quochuydev <quochuy.dev@gmail.com>
2025-09-24 17:36:01 +0700

    feat: neo4j

--- researching/spec-kit/src/agent.py
+++ researching/spec-kit/src/agent.py
@@ -5,6 +5,7 @@ nest_asyncio.apply()
 import asyncio
 from llama_index.core import (
     StorageContext,
+    SimpleDirectoryReader,
     Document,
 )
 from dotenv import load_dotenv
@@ -47,7 +48,58 @@ def insert(documents):
     )
 
 
+class SpecRelationship(BaseModel):
+    source: str = Field(description="The source entity ID, e.g. Booking")
+    target: str = Field(description="The target entity ID, e.g. Hotel")
+    type: str = Field(
+        description="The relationship type, e.g. HAS, BELONGS_TO, APPLIES_TO"
+    )
+
+
+class SpecEntity(BaseModel):
+    id: str = Field(description="Unique identifier for the entity, e.g. Booking")
+    fields: List[str] = Field(description="List of key fields for the entity")
+
+
+class SpecSchema(BaseModel):
+    entities: List[SpecEntity] = Field(
+        description="List of extracted entities from spec-kit"
+    )
+    relationships: List[SpecRelationship] = Field(
+        description="List of relationships between entities"
+    )
+
+
 async def main():
+    documents = SimpleDirectoryReader("./specs").load_data()
+
+    extractor = FunctionAgent(
+        system_prompt=(
+            """You are an assistant that extracts structured entities and relationships from spec-kit documents.
+        - Use camelCase for both nodes and relationships.
+        - Entity have Fields (like SQL).
+        - Stay consistent across runs."""
+        ),
+        output_cls=SpecSchema,
+        llm=llm,
+    )
+
+    # response = await extractor.run(documents[0].text)
+
+    #     response = await extractor.run(
+    #         """## Key Entities
+    # - **Booking**: Contains guest details, dates, room selection, and payment information
+    # - **Room Type**: Defines room categories and their fields (size, max occupancy, amenities)
+    # - **Rate Plan**: Includes pricing rules, cancellation policies, and included services
+    # - **Guest Profile**: Stores guest preferences and Booking history
+    # - **Hotel**: Contains location, contact information, and available amenities"""
+    #     )
+
+    #     print("Response:", response)
+
+    # parsed_entities: SpecSchema = response.get_pydantic_model(SpecSchema)
+    # print("Parsed Entities as Python object:", parsed_entities)
+
     documents = [
         Document(
             text="BookingApp project HAS_ENTITY: Booking, Room, Pricing, User",
@@ -112,11 +164,26 @@ async def main():
         ),
     ]
 
-    insert(documents)
+    try:
+        storage_context = StorageContext.from_defaults(persist_dir="./storage_n4")
 
-    kg_index = PropertyGraphIndex.from_existing(
-        llm=llm, property_graph_store=property_graph_store
-    )
+        kg_index = PropertyGraphIndex.from_existing(
+            llm=llm, property_graph_store=property_graph_store
+        )
+    except FileNotFoundError as e:
+        print("❌ Creating new index")
+        storage_context = StorageContext.from_defaults(graph_store=property_graph_store)
+
+        kg_index = PropertyGraphIndex.from_documents(
+            llm=llm,
+            documents=documents,
+            storage_context=storage_context,
+            max_triplets_per_chunk=10,
+            include_embeddings=True,
+            property_graph_store=property_graph_store,
+        )
+
+        storage_context.persist(persist_dir="./storage_n4")
 
     query_engine = kg_index.as_query_engine(
         include_text=True,
