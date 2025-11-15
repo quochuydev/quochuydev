0952ac088fb570dcc5e77e9592625a56901921be
quochuydev <quochuy.dev@gmail.com>
2025-11-15 12:39:27 +0700

    fix: blog

--- blog/vite.config.ts
+++ blog/vite.config.ts
@@ -8,7 +8,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/quochuydev/' : '/',
+  base: '/',
   plugins: [
     {
       enforce: 'pre',
